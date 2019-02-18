1aed96116d9947f83fb66046be6a152ae0a9a9e5
Nick Leiby <nick.leiby@twosixlabs.com>
2019-02-18 13:20:15 -0500

    Adds requirements and submission script

---
+++ make_perovskites_blank_submission.py
@@ -0,0 +1,85 @@
+import git
+import os
+import yaml
+import sys
+
+# handles python 2/3 compatibility
+from builtins import input
+
+
+def make_versioned_data_repo_path():
+    user_input = input("Enter the path of your file: ")
+    assert os.path.exists(user_input), "Unable to find a versioned repo directory at:  "+str(user_input)
+    with open('local_versioned_data_repo_path.py', 'w') as fout:
+        fout.write("repo_path = '%s'\n" % user_input)
+
+
+def get_versioned_data_repo_directory():
+    repo_path = None
+    try:
+        from local_versioned_data_repo_path import repo_path
+    except ModuleNotFoundError:
+        make_versioned_data_repo_path()
+    # try to import after making the versioned data path.  Should also catch bad user-provided paths in make function
+    if not repo_path:
+        get_versioned_data_repo_directory()
+    return repo_path
+
+
+
+def get_git_info(versioned_datasets_repo_path):
+    # get information from the git repo about the user and the current commit
+    repo = git.Repo(versioned_datasets_repo_path)
+    git_sha = repo.head.object.hexsha
+    reader = repo.config_reader()
+    git_username = reader.get_value("user", "name")
+    return git_sha, git_username
+
+
+def get_files_of_necessary_types(versioned_datasets_repo_path):
+    # get information from the perovskite manifest for the template
+    perovskite_manifest_filename = 'perovskite.manifest.yml'
+
+    with open(os.path.join(versioned_datasets_repo_path, 'manifest', perovskite_manifest_filename)) as mf:
+        perovskite_manifest = yaml.load(mf)
+
+    # the perovskite manifest should only have one each uncommented file of training data and stateset at a same time
+    file_names = perovskite_manifest['files']
+
+    file_types = {
+        'trainingdata': None,
+        'stateset': None,
+    }
+
+    for file_name in file_names:
+        # file_name_components = file_name.split('/')
+        # assert(len(file_name_components) == 2, 'A file with an unrecognized name format is in the manifest')
+        base_filename = os.path.basename(file_name)
+        versioned_file_type = base_filename.split('.')[-2]
+        if versioned_file_type in file_types and not file_types.get(versioned_file_type):
+            file_types[versioned_file_type] = file_name
+
+    for file_type, file_name in file_types.items():
+        if file_name is None:
+            raise ValueError("Unable to find a file in the perovskites manifest of type: %s" % file_type)
+    return file_types
+
+
+versioned_datasets_repo_path = get_versioned_data_repo_directory()
+file_types = get_files_of_necessary_types(versioned_datasets_repo_path)
+git_sha, git_username = get_git_info(versioned_datasets_repo_path)
+training_data_git_hash = git_sha[:7]
+state_set_git_hash = git_sha[:7]
+# file name formatted like 0015.trainingdata.csv
+crank_number = os.path.basename(file_types['trainingdata']).split('.')[0]
+submission_template_filename = '_'.join([crank_number, 'train', training_data_git_hash, 'stateset', state_set_git_hash]) + '.csv'
+
+
+# Create a submission template file in the locatin that the script was run
+with open(submission_template_filename, 'w') as fout:
+    fout.write("# USERNAME: %s\n" % git_username)
+    fout.write("# MODEL USED/ NOTES\n")
+    fout.write("# Data schema: <11 character hash of state type=str>, <run# type=int>, <truncated 5 decimal places type=float>,<truncated 5 decimal places type=float>, <1-4 type=int>, <0-1 (confidence) type=float\n")
+    fout.write("dataset,name,_rxn_M_inorganic,_rxn_M_organic,predicted_out,score\n")
+
+print("Created file %s" % submission_template_filename)
