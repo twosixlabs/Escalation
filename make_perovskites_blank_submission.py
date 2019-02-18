1ac63087a72dcf8f286d89e800e8ddfeb55eef1f
Nick Leiby <nick.leiby@twosixlabs.com>
2019-02-18 13:37:55 -0500

    Refactors make file into function

--- make_perovskites_blank_submission.py
+++ make_perovskites_blank_submission.py
@@ -71,28 +71,34 @@ def get_files_of_necessary_types(versioned_datasets_repo_path):
     return file_types
 
 
-versioned_datasets_repo_path = get_versioned_data_repo_directory()
-print("Using {} as versioned-datasets path.  Getting git commit from data there.".format(versioned_datasets_repo_path))
-file_types = get_files_of_necessary_types(versioned_datasets_repo_path)
-git_sha, git_username = get_git_info(versioned_datasets_repo_path)
-training_data_git_hash = git_sha[:7]
-# NB: they could have checked this code out without pulling the most recent dataset using the pull script,
-# but that should be caught by having a stale crank number
-print("Git commit of local versioned-datasets manifest: {}.  Git username: {}".format(git_sha, git_username))
-# file name formatted like 0015.trainingdata.csv
-crank_number = os.path.basename(file_types['trainingdata']).split('.')[0]
-git_username_no_whitespace = git_username.replace(' ', '')
-submission_template_filename = '_'.join([crank_number,
-                                         'train',
-                                         training_data_git_hash,
-                                         git_username_no_whitespace]) + '.csv'
+def make_submission_template_csv():
+    versioned_datasets_repo_path = get_versioned_data_repo_directory()
+    print("Using {} as versioned-datasets path.  Getting git commit from data there.".format(
+        versioned_datasets_repo_path))
+    file_types = get_files_of_necessary_types(versioned_datasets_repo_path)
+    git_sha, git_username = get_git_info(versioned_datasets_repo_path)
+    training_data_git_hash = git_sha[:7]
+    # NB: they could have checked this code out without pulling the most recent dataset using the pull script,
+    # but that should be caught by having a stale crank number
+    print("Git commit of local versioned-datasets manifest: {}.  Git username: {}".format(git_sha, git_username))
+    # file name formatted like 0015.trainingdata.csv
+    crank_number = os.path.basename(file_types['trainingdata']).split('.')[0]
+    git_username_no_whitespace = git_username.replace(' ', '')
+    submission_template_filename = '_'.join([crank_number,
+                                             'train',
+                                             training_data_git_hash,
+                                             git_username_no_whitespace]) + '.csv'
+
+    # Create a submission template file in the locatin that the script was run
+    with open(submission_template_filename, 'w') as fout:
+        fout.write("# USERNAME: %s\n" % git_username)
+        fout.write("# USER CAN ENTER COMMENT LINES WITH MODEL USED OR NOTES HERE\n")
+        fout.write(
+            "# Data schema: <11 character hash of state type=str>, <run# type=int>, <truncated 5 decimal places type=float>,<truncated 5 decimal places type=float>, <1-4 type=int>, <0-1 (confidence) type=float\n")
+        fout.write("dataset,name,_rxn_M_inorganic,_rxn_M_organic,predicted_out,score\n")
+
+    print("Created file %s" % submission_template_filename)
 
 
-# Create a submission template file in the locatin that the script was run
-with open(submission_template_filename, 'w') as fout:
-    fout.write("# USERNAME: %s\n" % git_username)
-    fout.write("# MODEL USED/ NOTES\n")
-    fout.write("# Data schema: <11 character hash of state type=str>, <run# type=int>, <truncated 5 decimal places type=float>,<truncated 5 decimal places type=float>, <1-4 type=int>, <0-1 (confidence) type=float\n")
-    fout.write("dataset,name,_rxn_M_inorganic,_rxn_M_organic,predicted_out,score\n")
-
-print("Created file %s" % submission_template_filename)
+if __name__ == '__main__':
+    make_submission_template_csv()
