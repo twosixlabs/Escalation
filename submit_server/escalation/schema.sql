3f9d4f12e8ac48c0bf9cb3666f98201b283380f2
snovotney <scott.novotney@twosixlabs.com>
2019-02-22 20:49:33 -0500

    Issue #10: add submission download selection

--- submit_server/escalation/schema.sql
+++ submit_server/escalation/schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS submission;
+DROP TABLE IF EXISTS cranks;
 
 CREATE TABLE submission (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,3 +11,18 @@ CREATE TABLE submission (
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+
+CREATE TABLE cranks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  crank TEXT NOT NULL,
+  stateset TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO cranks (crank,stateset) VALUES ("0001", "abc123");
+INSERT INTO cranks (crank,stateset) VALUES ("0002", "123abc");
+
+INSERT into submission (username, expname, crank, filename, notes) VALUES ("snovot","first","0001","abc.csv","test test");
+INSERT into submission (username, expname, crank, filename, notes) VALUES ("snovot","second","0001","abc.csv","test test");
+INSERT into submission (username, expname, crank, filename, notes) VALUES ("snovot","third","0002","abc.csv","test test");
+INSERT into submission (username, expname, crank, filename, notes) VALUES ("snovot","fourth","0002","abc.csv","test test");
--- submit_server/escalation/view.py
+++ submit_server/escalation/view.py
@@ -4,15 +4,31 @@ from flask import (
 )
 from flask import current_app as app
 from escalation.db import get_db
-
+from escalation.download import download_zip
 
 bp = Blueprint('view', __name__)
-@bp.route('/')
+@bp.route('/', methods=('GET','POST'))
+
 def view():
     db = get_db()
-    submissions = db.execute(
-        'SELECT id, username, crank, filename, notes, created '
-        ' FROM submission '
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('index.html', submissions=submissions)
+    cranks = db.execute("SELECT DISTINCT crank FROM cranks ORDER by crank DESC").fetchall()
+    cranks = [ x['crank'] for x in cranks]
+
+    crank = "all"
+    
+    if request.method == 'POST' and 'crank' in request.form:
+        crank = request.form['crank']
+    
+    if request.method == 'POST' and 'download' in request.form:
+        print("files",request.form.getlist('download'))
+
+        
+    if request.method == 'POST' and crank != 'all':
+        crank = request.form['crank']
+        query = "SELECT id, username, expname, crank, filename, notes, created FROM submission WHERE crank = '%s' ORDER BY created DESC" % crank
+    else:
+        query = "SELECT id, username, expname, crank, filename, notes, created FROM submission ORDER BY created DESC"
+        
+    submissions = db.execute(query).fetchall()
+            
+    return render_template('index.html', submissions=submissions,cranks=cranks, crank=crank)
