d7a373aa56d08ed35b35916e36dd2cf363e2f72a
snovotney <scott.novotney@twosixlabs.com>
2019-03-20 09:17:09 -0400

    Issue #12: skeleton to render and update dashboard tables

--- submit_server/escalation/__init__.py
+++ submit_server/escalation/__init__.py
@@ -10,85 +10,82 @@ import logging
 from logging.handlers import RotatingFileHandler
 # create and configure the app
 
-app = Flask(__name__, instance_relative_config=True)
-
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    #        DATABASE=os.path.join(app.instance_path, 'escalation.sqlite'),
-    UPLOAD_FOLDER = os.path.join(app.instance_path,'submissions'),
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(app.instance_path,'escalation.sqlite'),
-    #32 MB max upload
-    MAX_CONTENT_LENGTH = 32 * 1024 * 1024,
-    ADMIN_KEY='secret',
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
 scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
-
-# ensure the instance folder exists
-try:
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    if not os.path.exists(app.instance_path):
-        os.makedirs(app.instance_path)
-except OSError:
-    pass
-
-# a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
-
-@app.route('/links')
-def links():
-    return render_template('links.html')
-
-from . import submission
-from . import view
-from . import admin
-from . import dashboard
-
-app.register_blueprint(submission.bp)
-app.register_blueprint(view.bp)
-app.register_blueprint(admin.bp)            
-app.register_blueprint(dashboard.bp)
-
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/escalation.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('ESCALATion started')    
-
-app.logger.info("Writing to %s" % app.config['SQLALCHEMY_DATABASE_URI'])
-
-
-#@app.shell_context_processor
-#def make_shell_context():
-#    return {'db': db, 'Run':database}
-
-from .database import insert_demo_data, delete_db, create_db, Run, Submission, Crank, Prediction
-
-@app.cli.command('reset-db')
-def reset_db():
-    delete_db()
-    click.echo("Deleted db entries")
-
-@app.cli.command('demo-db')
-def demo_data():
-    insert_demo_data()
-    click.echo("Added demo data")    
-
-
-# Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        #        DATABASE=os.path.join(app.instance_path, 'escalation.sqlite'),
+        UPLOAD_FOLDER = os.path.join(app.instance_path,'submissions'),
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(app.instance_path,'escalation.sqlite'),
+        #32 MB max upload
+        MAX_CONTENT_LENGTH = 32 * 1024 * 1024,
+        ADMIN_KEY='secret',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    
+    db.init_app(app)
+    migrate.init_app(app,db)
+
+    if not scheduler.running:
+        scheduler.init_app(app)        
+        scheduler.start()
+    
+    # ensure the instance folder exists
+    try:
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+        if not os.path.exists(app.instance_path):
+            os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from .submission import bp as sub_bp
+    app.register_blueprint(sub_bp)
+    
+    from .view import bp as view_bp
+    app.register_blueprint(view_bp)    
+    from .admin import bp as admin_bp
+    app.register_blueprint(admin_bp)
+
+    from . import dashboard    
+    app.register_blueprint(dashboard.bp)
+    
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/escalation.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('ESCALATion started')    
+
+        app.logger.info("Writing to %s" % app.config['SQLALCHEMY_DATABASE_URI'])
+
+
+    from .database import insert_demo_data, delete_db, create_db, Run, Submission, Crank, Prediction
+
+    @app.cli.command('reset-db')
+    def reset_db():
+        delete_db()
+        click.echo("Deleted db entries")
+
+    @app.cli.command('demo-db')
+    def demo_data():
+        insert_demo_data()
+        click.echo("Added demo data")    
+
+        
+    # Shut down the scheduler when exiting the app
+    return app
+
--- submit_server/escalation/dashboard.py
+++ submit_server/escalation/dashboard.py
@@ -1,13 +1,14 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
+    Blueprint, flash, g, redirect, render_template, request
 )
 from flask import current_app
 from flask_table import Table, Col
 from . import database as db
-from apscheduler.schedulers.background import BackgroundScheduler
+from escalation import scheduler
 import time
 
 bp = Blueprint('dashboard', __name__)
+
 
 class StatsTable(Table):
     name = Col('Summary')
@@ -18,45 +19,43 @@ class Stat(object):
     def __init__(self, name, value):
         self.name = name
         self.value = value
+        
+global science_table
+global ml_table
+global auto_table
 
-with app.app_context():        
-    g.science_table = StatsTable([Stat(name='Blank mean',value=0.0),Stat(name='std',value=0.2)])
-    g.ml_table = StatsTable([Stat(name='Blank mean',value=0.0),Stat(name='std',value=0.0)])
-    g.auto_table = StatsTable([Stat(name='Blank mean',value=0.0),Stat(name='std',value=0.0)])
+science_table = StatsTable([Stat(name='Blank mean',value=0.0),Stat(name='std',value=0.2)])
+ml_table = StatsTable([Stat(name='Blank mean',value=0.0),Stat(name='std',value=0.0)])
+auto_table = StatsTable([Stat(name='Blank mean',value=0.0),Stat(name='std',value=0.0)])
 
 def update_science():
-    current_app.logger.info("Updating science stats")
-    sleep(5)    
-    g.science_table = StatsTable([Stat(name='Science mean',value=0.2),Stat(name='std',value=0.2)])
+    global science_table
+    science_table = StatsTable([Stat(name='Science mean',value=0.2),Stat(name='std',value=0.2)])
 
 def update_auto():
-    current_app.logger.info("Updating automation stats")
-    sleep(5)
-    g.auto_table = StatsTable([Stat(name='auto mean',value=0.2),Stat(name='std',value=0.2)])    
+    global auto_table
+    auto_table = StatsTable([Stat(name='auto mean',value=0.2),Stat(name='std',value=0.2)])    
 
 def update_ml():
-    current_app.logger.info("Updating ML stats")
-    sleep(2)    
-    g.ml_table = StatsTable([Stat(name='ml mean',value=0.2),Stat(name='std',value=0.2)])    
+    global ml_table
+    ml_table = StatsTable([Stat(name='ml mean',value=0.2),Stat(name='std',value=0.2)])
     
 @bp.route('/dashboard', methods=('GET','POST'))
 def dashboard():
-
-
     if request.method == 'POST':
         if 'update_science' in request.form:
             flash("Refreshing Science stats...")
-            job = current_app.apscheduler.add_job(func=update_science, args=[], id = 'update_science')
+            job = scheduler.add_job(func=update_science, args=[], id = 'update_science')
         elif 'update_auto' in request.form:
             flash("Refreshing Automation stats...")
-            job =current_app.apscheduler.add_job(func=update_auto, args=[], id = 'update_auto')            
+            job =scheduler.add_job(func=update_auto, args=[], id = 'update_auto')            
         elif 'update_ml' in request.form:
             flash("Refreshing ML stats...")            
-            job = current_app.apscheduler.add_job(func=update_ml, args=[], id = 'update_ml')            
+            job = scheduler.add_job(func=update_ml, args=[], id = 'update_ml')            
         else:
             flash("Unknown button!")
 
         flash("Kicked off job {}".format(job))
-            
+        
     return render_template('dashboard.html',science_table=science_table,auto_table=auto_table,ml_table=ml_table)
 
