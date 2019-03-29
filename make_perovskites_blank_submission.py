69c734ebef982c0067a61db9b2a64e4c3d177f04
snovotney <scott.novotney@twosixlabs.com>
2019-03-29 15:41:28 -0400

    allow multiple cranks in versioned data

--- make_perovskites_blank_submission.py
+++ make_perovskites_blank_submission.py
@@ -21,34 +21,29 @@ def get_submission_template_info():
     versioned_datasets_repo_path = utils.get_versioned_data_repo_directory()
     print("Using {} as versioned-datasets path.  Getting git commit from data there.".format(
         versioned_datasets_repo_path))
-    file_types = utils.get_files_of_necessary_types(versioned_datasets_repo_path,args.debug)
+    files = utils.get_files_of_necessary_types(versioned_datasets_repo_path,args.debug)
     git_sha, git_username = utils.get_git_info(versioned_datasets_repo_path)
     training_data_git_hash = git_sha[:7]
-    # NB: they could have checked this code out without pulling the most recent dataset using the pull script,
-    # but that should be caught by having a stale crank number
+
     print("Git commit of local versioned-datasets manifest: {}.  Git username: {}".format(git_sha, git_username))
     # file name formatted like 0015.trainingdata.csv
-    crank_number = os.path.basename(file_types['perovskitedata']).split('.')[0]
+
     git_username_no_whitespace = git_username.replace(' ', '')
-    submission_template_filename = '_'.join([crank_number,
-                                             'train',
-                                             training_data_git_hash,
-                                             git_username_no_whitespace]) + '.csv'
-    return submission_template_filename, git_username
+    for crank_number in files:    
+        submission_template_filename = '_'.join([crank_number,
+                                                 'train',
+                                                 training_data_git_hash,
+                                                 git_username_no_whitespace]) + '.csv'
 
-
-def make_submission_template_csv():
-    submission_template_filename, git_username = get_submission_template_info()
-    # Create a submission template file in the locatin that the script was run
-    with open(submission_template_filename, 'w') as fout:
-        fout.write("# USERNAME: %s\n" % git_username)
-        fout.write("# USER CAN ENTER COMMENT LINES WITH MODEL USED OR NOTES HERE\n")
-        fout.write(
-            "# Data schema: <11 character md5 hash of state type=str>, <run# type=int>, <truncated 5 decimal places type=float>,<truncated 5 decimal places type=float>,<truncated 5 decimal places type=float>, <1-4 type=int>, <0-1 (confidence) type=float\n")
-        fout.write("dataset,name,_rxn_M_inorganic,_rxn_M_organic,_rxn_M_acid,predicted_out,score\n")
-
-    print("Created file %s" % submission_template_filename)
+        # Create a submission template file in the location that the script was run
+        with open(submission_template_filename, 'w') as fout:
+            fout.write("# USERNAME: %s\n" % git_username)
+            fout.write("# USER CAN ENTER COMMENT LINES WITH MODEL USED OR NOTES HERE\n")
+            fout.write(
+                "# Data schema: <11 character md5 hash of state type=str>, <run# type=int>, <truncated 5 decimal places type=float>,<truncated 5 decimal places type=float>,<truncated 5 decimal places type=float>, <1-4 type=int>, <0-1 (confidence) type=float\n")
+            fout.write("dataset,name,_rxn_M_inorganic,_rxn_M_organic,_rxn_M_acid,predicted_out,score\n")
+            print("Created file %s" % submission_template_filename)
 
 
 if __name__ == '__main__':
-    make_submission_template_csv()
+    get_submission_template_info()
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import git
 import os
 import yaml
+import re
+from collections import defaultdict
 
 def make_versioned_data_repo_path():
     user_input = input("Enter the absolute path of your versioned-datsets repo (e.g., /Users/nick.leiby/versioned-datasets/): ")
@@ -51,25 +53,25 @@ def get_files_of_necessary_types(versioned_datasets_repo_path,debug=False):
         print("!!\n!!\n!! Unable to find perovskites manifest.  Try changing the versioned-datasets repo path in local_versioned_data_repo_path.py or deleting the file to resolve.\n")
         raise
 
-    # the perovskite manifest should only have one each uncommented file of training data and stateset at a same time
     file_names = perovskite_manifest['files']
 
-    file_types = {
-        'perovskitedata': None,
-        'stateset': None,
-    }
-
+    file_types = ['perovskitedata','stateset']
+    found_files = defaultdict(lambda: defaultdict(str))
     for file_name in file_names:
         # file_name_components = file_name.split('/')
         # assert(len(file_name_components) == 2, 'A file with an unrecognized name format is in the manifest')
         base_filename = os.path.basename(file_name)
         versioned_file_type = base_filename.split('.')[-2]
-        if versioned_file_type in file_types and not file_types.get(versioned_file_type):
-            file_types[versioned_file_type] = os.path.join(versioned_datasets_repo_path,'data',perovskite_manifest['project name'],file_name)
+        crank  = re.findall(r'(\d{4})\.', base_filename)[0]
 
-    for file_type, file_name in file_types.items():
-        if file_name is None:
-            raise ValueError("Unable to find a file in the perovskites manifest of type: %s" % file_type)
-    return file_types
+        if versioned_file_type in file_types:
+            found_files[crank][versioned_file_type] = os.path.join(versioned_datasets_repo_path,'data',perovskite_manifest['project name'],file_name)
+
+
+    for crank in found_files:
+        for ft in file_types:
+            if found_files[crank][ft] is None:
+                raise Exception("Could not find %s for crank %s" %(ft, crank))
+    return found_files
 
 
