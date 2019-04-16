99ec79316d12caadd9efbc24b31a202537dfecb7
snovotney <scott.novotney@twosixlabs.com>
2019-04-15 22:07:41 -0400

    increment version

--- submit_server/build.sh
+++ submit_server/build.sh
@@ -1,3 +1,3 @@
 docker build -t escalation-server .
-docker tag escalation-server snovotney/escalation-server:0.9.2o
+docker tag escalation-server snovotney/escalation-server:0.9.5
 docker push snovotney/escalation-server
