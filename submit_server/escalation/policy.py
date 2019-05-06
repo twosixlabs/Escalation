836aa29b04303271fc03bb3268576490094b701a
snovotney <scott.novotney@twosixlabs.com>
2019-05-06 17:40:03 -0400

    initial code for dl w/uniform sample

---
+++ submit_server/escalation/policy.py
@@ -0,0 +1,67 @@
+from flask import current_app as app
+import shutil
+from tempfile import mkdtemp
+import os
+import csv
+import random
+import time
+
+def download_uniform_policy(basedir,submissions,size,pfx=""):
+    app.logger.info("Running uniform sampling policy")
+    target_size = size // len(submissions)
+    targets=[]
+    remainder = size % len(submissions)
+    for i in range(len(submissions)):
+        targets.append(target_size + 1 if i < remainder else target_size)
+    for i in range(len(targets)):
+        app.logger.info("Selected %d rows for %s" % (targets[i], submissions[i].expname))
+        
+    tmpdir = mkdtemp()
+    metadata=[]
+    for sub in submissions:
+        metadata.append({'user':sub.username,
+                         'id': sub.id,
+                         'crank':sub.crank,
+                         'created':sub.created,
+                         'expname':sub.expname,
+                         'notes':sub.notes,
+        })
+        
+    with open(os.path.join(tmpdir,'metadata.csv'),'w') as csvfile:
+        fieldnames = ['id','user','crank','created','expname']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,lineterminator='\n',extrasaction='ignore')
+        writer.writeheader()
+        for elem in metadata:
+            writer.writerow(elem)
+
+    seen={}
+    with open(os.path.join(tmpdir,'submission.csv'),'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=['dataset','name','predicted_out','score','expname'],extrasaction='ignore',lineterminator='\n')        
+        writer.writeheader()
+        for idx, sub in enumerate(submissions):
+            preds = [p.__dict__ for p in sub.rows] #map to dict for csv.dictwriter
+            random.shuffle(preds)
+            c = 0
+            for p in preds:
+                if p['name'] in seen:
+                    continue
+                seen[p['name']] = 1
+                if p['predicted_out'] == 0:
+                    p['predicted_out'] = 'null'
+
+                p['expname'] = sub.expname                    
+                writer.writerow(p)
+                c+=1                
+                if c >= targets[idx]:
+                    break
+            app.logger.info("Writing %d of %d predictions for %s" % (c,len(preds), sub))
+            
+    if pfx:
+        curr = "escalation." + str(pfx) + "." + time.strftime("%Y%m%d-%H%M%S",time.localtime())
+    else:
+        curr = "escalation." + time.strftime("%Y-%m-%d-%H%M%S",time.localtime())
+        
+    shutil.make_archive(os.path.join(basedir,curr),'zip',tmpdir)
+    shutil.rmtree(tmpdir)
+
+    return os.path.join(basedir,curr+".zip"), "Selected %d predictions per %d models, resulting in %d total" %(target_size, len(submissions), len(seen))
--- submit_server/escalation/view.py
+++ submit_server/escalation/view.py
@@ -6,6 +6,7 @@ from flask import (
 from flask import current_app as app
 from . import database as db
 from .files import download_zip
+from .policy import download_uniform_policy
 from .dashboard import update_auto, update_science
 from escalation import scheduler
 
@@ -13,8 +14,10 @@ bp = Blueprint('view', __name__)
 @bp.route('/', methods=('GET','POST'))
 
 def view():
-    curr_crank = "all"
-    
+    cranks=db.get_unique_cranks()
+    curr_crank = cranks[0]
+    models=db.get_submissions(curr_crank)
+
     if request.method == 'POST' and 'crank' in request.form:
         curr_crank = request.form['crank']
 
@@ -30,9 +33,38 @@ def view():
     submissions=db.get_submissions(curr_crank)
 
     if request.method == 'POST' and  'submit' in request.form and request.form['submit'] == 'Download files':
+        
         requested=[int(x) for x in request.form.getlist('download')]
         submissions = [sub for sub in submissions if sub.id in requested]
         zipfile = download_zip(app.config['UPLOAD_FOLDER'],submissions, curr_crank)
         return send_file(os.path.join(app.config['UPLOAD_FOLDER'],zipfile),as_attachment=True)
+
+    if request.method == 'POST' and 'policy_submit' in request.form:
+        policy_crank = request.form['policy_crank']
+        size         = request.form['cranksize']        
+        submissions  = db.get_submissions(policy_crank)        
+        requested    = [ int(x) for x in request.form.getlist('policy_download')]
+        submissions  = [sub for sub in submissions if sub.id in requested]
+        err = None
+        try:
+            size = int(size)
+        except:
+            err = "Passed in value '%s' for number of samples is not an integer"
+        if size < 1:
+            err = "Number of samples must be greater than 0"
+        elif len(requested) == 0:
+            err = "Must select a model to include"
+
+        if err:
+            flash(err)
+        else:
+            zipfile, explanation = download_uniform_policy(app.config['UPLOAD_FOLDER'],submissions,size, policy_crank)
+            flash(explanation)
+            app.logger.info(explanation)
+            return send_file(os.path.join(app.config['UPLOAD_FOLDER'],zipfile),as_attachment=True)
+        
+    elif request.method == 'POST' and 'policy_crank' in request.form:
+        curr_crank = request.form['policy_crank']
+        models = db.get_submissions(curr_crank)
     
-    return render_template('index.html',submissions=submissions,cranks=db.get_unique_cranks(), crank=curr_crank)
+    return render_template('index.html',submissions=submissions,cranks=cranks, curr_crank=curr_crank, models=models)
