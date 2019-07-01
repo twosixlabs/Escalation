b4634c09df69d66c7086a6bb3a8b1dfe99183b48
Nick Leiby <nick.leiby@twosixlabs.com>
2019-07-01 15:48:10 -0400

    New requirements file.  PEP-8 formatting

--- submit_server/escalation/submission.py
+++ submit_server/escalation/submission.py
@@ -9,15 +9,16 @@ from flask import (
 from flask import current_app as app
 from werkzeug.utils import secure_filename
 
-from .  import database as db
+from . import database as db
 from .files import *
 from .dashboard import update_ml, update_auto, update_science
 from escalation import scheduler
 
+
 def arr2html(arr):
     if type(arr) == str:
         return arr
-    out="<ul>\n"
+    out = "<ul>\n"
     out += "\n".join("<li>%s</li>" % x for x in arr)
     out += "\n</ul>\n"
     return out
@@ -28,80 +29,78 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ['csv']
 
 
-
 bp = Blueprint('submission', __name__)
+
 
 @bp.route('/submission', methods=('GET', 'POST'))
 def submission():
-
     if request.method == 'POST':
 
-
-        if request.headers.get('User-Agent') != 'escalation':        
-            for key in ('username','expname','crank','notes','githash'):
+        if request.headers.get('User-Agent') != 'escalation':
+            for key in ('username', 'expname', 'crank', 'notes', 'githash'):
                 session[key] = request.form[key]
         else:
-            for key in ('username','expname','crank','notes','githash'):
+            for key in ('username', 'expname', 'crank', 'notes', 'githash'):
                 if key not in request.form:
-                    return jsonify({'error':"Key %s not presente in POST" % key}), 400
-                
-        username = request.form['username']
-        expname  = request.form['expname']       
-        crank    = request.form['crank']
-        notes    = request.form['notes']
-        githash    = request.form['githash']                
-        csvfile  = request.files['csvfile']
+                    return jsonify({'error': "Key %s not presente in POST" % key}), 400
 
-        app.logger.info("POST {} {} {} {} {}".format(username,expname,crank,notes,csvfile.filename))
+        username = request.form['username']
+        expname = request.form['expname']
+        crank = request.form['crank']
+        notes = request.form['notes']
+        githash = request.form['githash']
+        csvfile = request.files['csvfile']
+
+        app.logger.info("POST {} {} {} {} {}".format(username, expname, crank, notes, csvfile.filename))
         error = None
-        
+
         if not username:
             error = 'Username is required.'
         if not expname:
-            error = 'Experiment name is required.'            
+            error = 'Experiment name is required.'
         elif not crank:
             error = 'Crank number is required (e.g. 0015)'
         elif not githash or len(githash) != 7:
             error = '7 char git hash is required (e.g. abc1234)'
         elif not csvfile or not allowed_file(csvfile.filename):
             error = "Must upload a csv file"
-        elif not db.is_stateset_active(crank,githash):
+        elif not db.is_stateset_active(crank, githash):
             error = "Entered crank number %s is not active" % (crank)
         else:
             rows, comments = text2rows(csvfile.read().decode('utf-8'))
             if rows == None:
                 error = "Could not extract rows from file %s" % csvfile.filename
             if error == None:
-                error = validate_submission(rows,crank,githash)
+                error = validate_submission(rows, crank, githash)
             if error == None:
                 if comments:
                     notes = notes + "\nComments from file:" + comments
-                error = db.add_submission(username,expname,crank,githash,rows,notes)
+                error = db.add_submission(username, expname, crank, githash, rows, notes)
 
-        #custom check if POST from script instead of UI
+        # custom check if POST from script instead of UI
         if error:
-            #turn into array for downstream printing -- bleh I know
+            # turn into array for downstream printing -- bleh I know
             if type(error) == str:
                 error = [error]
             app.logger.info("\n".join(error))
             if request.headers.get('User-Agent') == 'escalation':
-                return jsonify({'error':error}), 400
+                return jsonify({'error': error}), 400
             else:
                 flash(arr2html(error))
         else:
             app.logger.info("Added submission")
             # kick off stats refresh
-            job1 = scheduler.add_job(func=update_science, args=[], id = 'update_science')
-            job2 = scheduler.add_job(func=update_auto, args=[], id = 'update_auto')
-            job3 = scheduler.add_job(func=update_ml, args=[], id = 'update_ml')                    
-            
+            job1 = scheduler.add_job(func=update_science, args=[], id='update_science')
+            job2 = scheduler.add_job(func=update_auto, args=[], id='update_auto')
+            job3 = scheduler.add_job(func=update_ml, args=[], id='update_ml')
+
             if request.headers.get('User-Agent') == 'escalation':
-                return jsonify({'success':'Added submission'})
+                return jsonify({'success': 'Added submission'})
             else:
-                #clear out session
-                for key in ('username','expname','crank','notes','githash'):
-                    session.pop(key,None)
-                return render_template('success.html',username=username)
+                # clear out session
+                for key in ('username', 'expname', 'crank', 'notes', 'githash'):
+                    session.pop(key, None)
+                return render_template('success.html', username=username)
 
     curr_cranks = " ".join(sorted(x.crank for x in db.get_active_cranks()))
-    return render_template('submission.html',session=session,curr_cranks=curr_cranks)
+    return render_template('submission.html', session=session, curr_cranks=curr_cranks)
--- submit_server/escalation/view.py
+++ submit_server/escalation/view.py
@@ -10,47 +10,48 @@ from .policy import download_uniform_policy, default_models
 from .dashboard import update_auto, update_science
 from escalation import scheduler
 
-
 bp = Blueprint('view', __name__)
-@bp.route('/', methods=('GET','POST'))
 
+
+@bp.route('/', methods=('GET', 'POST'))
 def view():
-    cranks=db.get_unique_cranks()
+    cranks = db.get_unique_cranks()
     curr_crank = 'all'
     policy_crank = cranks[0]
-    models=db.get_submissions(policy_crank)
+    models = db.get_submissions(policy_crank)
 
     if request.method == 'POST' and 'crank' in request.form:
         curr_crank = request.form['crank']
-        
+
     if request.method == 'POST' and 'submit' in request.form and request.form['submit'] == 'Delete file':
         if request.form['adminkey'] != app.config['ADMIN_KEY']:
             flash("Incorrect admin code")
         else:
-            requested=[int(x) for x in request.form.getlist('download')]
+            requested = [int(x) for x in request.form.getlist('download')]
             for id in requested:
                 db.remove_submission(id)
-        job2 = scheduler.add_job(func=update_auto, args=[], id = 'update_auto')        
+        job2 = scheduler.add_job(func=update_auto, args=[], id='update_auto')
 
-    submissions=db.get_submissions(curr_crank)
+    submissions = db.get_submissions(curr_crank)
 
-    if request.method == 'POST' and  'submit' in request.form and request.form['submit'] == 'Download files':
-        requested=[int(x) for x in request.form.getlist('download')]
+    if request.method == 'POST' and 'submit' in request.form and request.form['submit'] == 'Download files':
+        requested = [int(x) for x in request.form.getlist('download')]
         submissions = [sub for sub in submissions if sub.id in requested]
-        app.logger.info("Downloading %d submissions of %d requested for crank %s" % (len(submissions), len(requested),curr_crank))
-        zipfile = download_zip(app.config['UPLOAD_FOLDER'],submissions, curr_crank)
-        return send_file(os.path.join(app.config['UPLOAD_FOLDER'],zipfile),as_attachment=True)
+        app.logger.info(
+            "Downloading %d submissions of %d requested for crank %s" % (len(submissions), len(requested), curr_crank))
+        zipfile = download_zip(app.config['UPLOAD_FOLDER'], submissions, curr_crank)
+        return send_file(os.path.join(app.config['UPLOAD_FOLDER'], zipfile), as_attachment=True)
 
     if request.method == 'POST' and 'policy_submit' in request.form:
         policy_crank = request.form['policy_crank']
-        size         = request.form['cranksize']        
-        submissions  = db.get_submissions(policy_crank)        
-        requested    = [ int(x) for x in request.form.getlist('policy_download')]
-        submissions  = [sub for sub in submissions if sub.id in requested]
+        size = request.form['cranksize']
+        submissions = db.get_submissions(policy_crank)
+        requested = [int(x) for x in request.form.getlist('policy_download')]
+        submissions = [sub for sub in submissions if sub.id in requested]
         err = None
         try:
             size = int(size)
-        except:
+        except ValueError:
             err = "Passed in value '%s' for number of samples is not an integer"
         if size < 1:
             err = "Number of samples must be greater than 0"
@@ -60,12 +61,13 @@ def view():
         if err:
             flash(err)
         else:
-            zipfile, explanation = download_uniform_policy(app.config['UPLOAD_FOLDER'],submissions,size, policy_crank)
+            zipfile, explanation = download_uniform_policy(app.config['UPLOAD_FOLDER'], submissions, size, policy_crank)
             flash(explanation)
             app.logger.info(explanation)
-            return send_file(os.path.join(app.config['UPLOAD_FOLDER'],zipfile),as_attachment=True)
-        
+            return send_file(os.path.join(app.config['UPLOAD_FOLDER'], zipfile), as_attachment=True)
+
     elif request.method == 'POST' and 'policy_crank' in request.form:
         policy_crank = request.form['policy_crank']
         models = db.get_submissions(policy_crank)
-    return render_template('index.html',submissions=submissions,cranks=cranks, curr_crank=curr_crank, models=models,policy_crank=policy_crank,defaults=default_models)
+    return render_template('index.html', submissions=submissions, cranks=cranks, curr_crank=curr_crank, models=models,
+                           policy_crank=policy_crank, defaults=default_models)
