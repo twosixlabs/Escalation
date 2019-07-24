ffc52a6ef221916270288d3aa85bb66bfb802685
Nick Leiby <nick.leiby@twosixlabs.com>
2019-07-24 14:16:05 -0400

    Loads version from python file

--- settings.py
+++
@@ -1,3 +0,0 @@
-from VERSION import version
-from local_versioned_data_repo_path import repo_path
-from local_escalation_server_persistent_storage_path import escalation_persistent_data_path
---
+++ submit_server/VERSION.py
@@ -0,0 +1,4 @@
+version = "1.1.0"
+
+if __name__ == '__main__':
+    print(version)
