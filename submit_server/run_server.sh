fc918194c194a5145da157f9359344e52ae94c45
Nick Leiby <nick.leiby@twosixlabs.com>
2019-07-24 14:25:52 -0400

    Comments for run_server

--- submit_server/run_server.sh
+++ submit_server/run_server.sh
@@ -1,13 +1,13 @@
-docker run --name escalation -it --rm -p 8000:5000 --rm -e SECRET_KEY=perovskites-rule \
-    --link escalation-mysql:dbserver \
-    -e DATABASE_URL=mysql+pymysql://escalation:perovskites@dbserver/escalation \
-    escalation-server:latest
 
+ # link the db container to the app
+ # mount a local directory as a volume
+ # define where the flask app can find the persistent data volume
 
 docker run --name escalation -it --rm \
     -e TZ=`ls -la /etc/localtime | cut -d/ -f8-9` \
-    -e DATABASE_URL=mysql+pymysql://escalation:perovskites@dbserver/escalation \
-    -e ESCALATION_PERSISTENT_DATA_PATH=/persistent_data/escalation_data \
+    -e DATABASE_URL=mysql+pymysql://escalation:perovskites@dbserver/escalation --link escalation-mysql:dbserver \
     -v /Users/nick.leiby/escalation_data:/persistent_data/escalation_data \
-    -p 8000:5000 --rm -e SECRET_KEY=perovskites-rule --link escalation-mysql:dbserver \
+    -e ESCALATION_PERSISTENT_DATA_PATH=/persistent_data/escalation_data \
+    -e SECRET_KEY=perovskites-rule \
+    -p 8000:5000 --rm \
     escalation-server:latest
