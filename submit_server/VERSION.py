2381f03b47c00a1e7df85ce942235166c930fa16
Nick Leiby <nick.leiby@twosixlabs.com>
2019-08-16 12:21:06 -0400

    Bumps version to 1.3.0

--- submit_server/VERSION.py
+++ submit_server/VERSION.py
@@ -1,4 +1,7 @@
-version = "1.2.0"
+version = "1.3.0"
+
+# 1.3.0 Breaks dashboard into 3 parts.  Refactors views for clarity
+# 1.2.0 Adds download training data functionality
 
 if __name__ == '__main__':
     print(version)
