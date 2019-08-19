8c2e187017573060babd87a1eeb58ee16e258c59
Nick Leiby <nick.leiby@twosixlabs.com>
2019-08-19 16:02:33 -0400

    Removes delete db option

--- submit_server/escalation/VERSION.py
+++ submit_server/escalation/VERSION.py
@@ -1,5 +1,6 @@
-version = "1.3.0"
+version = "1.3.1"
 
+# 1.3.1 Removes delete db button.  Fixes dropdown carat.
 # 1.3.0 Breaks dashboard into 3 parts.  Refactors views for clarity (changing some endpoint names).  Removes reproducibility table and submissions by crank.
 # 1.2.0 Adds download training data functionality
 
