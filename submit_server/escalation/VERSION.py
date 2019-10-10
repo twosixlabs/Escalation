042c5ad0ca6c91dee66e22020b3dd501caeae6fb
snovotney <scott.novotney@twosixlabs.com>
2019-10-10 16:59:36 -0400

    fixup env variables

--- submit_server/escalation/VERSION.py
+++ submit_server/escalation/VERSION.py
@@ -1,5 +1,5 @@
-version = "1.5.0"
-
+version = "1.5.1"
+# 1.5.1 Adds leave one amine out UI
 # 1.5.0 Adds leave out amine out support
 # 1.4.0 Adds Shekar's interactive reaction space point plot
 # 1.3.1 Removes delete db button.  Fixes dropdown carat.
